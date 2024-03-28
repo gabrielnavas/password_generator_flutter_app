b11509d1f1ace209d972ed522d68d5f99064d460
navas <gabrielnavas@email.com>
2024-03-28 19:18:40 -0300

    fix decimal to 0

--- lib/widget/password_length_form.dart
+++ lib/widget/password_length_form.dart
@@ -45,7 +45,7 @@ class PasswordLengthForm extends StatelessWidget {
                 min: 1.0,
                 max: 50.0,
                 divisions: 50,
-                label: passwordLength.toStringAsFixed(2),
+                label: passwordLength.toString(),
                 onChanged: onChange,
               ),
             ),
