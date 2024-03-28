c16eabdaafcc6ccd7be795f1bc0711cd0d60c12d
navas <gabrielnavas@email.com>
2024-03-28 18:50:06 -0300

    add duration default

--- lib/messages/show_message.dart
+++ lib/messages/show_message.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ShowMessage {
-  static void showMessageSnack(BuildContext context, String message) {
+  static void showMessageSnack(
+    BuildContext context,
+    String message, {
+    Duration duration = const Duration(seconds: 3),
+  }) {
     final snackBar = SnackBar(
+      duration: duration,
       content: Text(
         message,
         style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
       ),
     );
 
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
